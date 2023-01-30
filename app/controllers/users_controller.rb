a269b709b049e2dfebf31d3c9799a19dc1fd98d9
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-01-30 23:39:27 +0900

    add view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "User was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :year, :zemi, :work)
+  end
 end
